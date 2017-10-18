feedbc11f2d5158236d073bd89c788cb22c0ce7f
Priyankit Bangia <priyankit.bangia@gmail.com>
2017-10-18 20:46:38 +1300

    Compile the test for getting the issue list from the forum service

---
+++ src/main/java/Clustering/ForumService.java
@@ -0,0 +1,19 @@
+package Clustering;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by priyankitbangia on 18/10/17.
+ */
+public class ForumService {
+
+
+    public ForumService(){
+
+    }
+
+    public List<String> getIssueTitles() {
+        return new ArrayList<>();
+    }
+}
