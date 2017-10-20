c547e2564a3554930dc9c6509064e6c0fb137778
Priyankit Bangia <priyankit.bangia@gmail.com>
2017-10-21 11:26:06 +1300

    Add all fields to cluster model

--- src/main/java/models/Cluster.java
+++ src/main/java/models/Cluster.java
@@ -16,8 +16,13 @@ public class Cluster implements Serializable {
 
     @Id
     private int clusterID;
-
     Set<Integer> postIDs = new HashSet<>();
+    private String title;
+    private String summary;
+    private int numUsers;
+    private String context;
+    private Set<String> assigneeIDs;
+    private String status;
 
     public Cluster(int id){
         this.clusterID = id;
@@ -33,6 +38,54 @@ public class Cluster implements Serializable {
 
     public Set<Integer> getPostIDs(){
         return postIDs;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public int getNumUsers() {
+        return numUsers;
+    }
+
+    public void setNumUsers(int numUsers) {
+        this.numUsers = numUsers;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public Set<String> getAssigneeIDs() {
+        return assigneeIDs;
+    }
+
+    public void setAssigneeIDs(Set<String> assigneeIDs) {
+        this.assigneeIDs = assigneeIDs;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     @Override
