fe5e46f624390df0e84a1b4d59f53e62d732329e
Priyankit Bangia <priyankit.bangia@gmail.com>
2017-10-21 16:56:46 +1300

    Create service access layer, provide login and forum services from one object

---
+++ src/main/java/app/IssueTracker.java
@@ -0,0 +1,65 @@
+package app;
+
+import Authentication.LoginService;
+import Clustering.ForumService;
+import com.mongodb.MongoClient;
+import models.Cluster;
+import models.UserStatus;
+import org.mongodb.morphia.Morphia;
+
+import java.net.UnknownHostException;
+
+/**
+ * Created by priyankitbangia on 21/10/17.
+ */
+public class IssueTracker {
+
+    private ForumService forumService;
+    private LoginService loginService;
+    //private User currentUser;
+
+    public IssueTracker() throws UnknownHostException{
+        this(new MongoClient("localhost", 27017), new Morphia());
+    }
+
+    public IssueTracker(MongoClient connection, Morphia morphia) {
+
+        forumService = new ForumService(connection, morphia);
+        loginService = new LoginService(connection, morphia);
+    }
+
+    public ForumService getForumService() {
+        //if (loginService.u)
+        return forumService;
+    }
+
+    public void setForumService(ForumService forumService) {
+        this.forumService = forumService;
+    }
+
+    public LoginService getLoginService() {
+        return loginService;
+    }
+
+    public void setLoginService(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    public UserStatus checkUserLoggedIn(String username){
+        return loginService.checkStatus(username);
+    }
+
+
+    public  void checkCurrentUserRole() {
+        return;
+    }
+
+    public Cluster getCluster(int i) {
+        //
+        return forumService.getCluster(i);
+    }
+
+    public void authenticate(String username, String password) {
+        //currentUser = loginService.login(username,password);
+    }
+}
