e4e2fd2a4c19373ad02ef65a02317343ed65337f
zyu539 <lhy@wifi-172-23-93-101.uoa-wifi.auckland.ac.nz>
2017-10-22 06:24:23 +0800

    Add tests for assigning issues

---
+++ src/test/java/Assignment/AssignmentTest.java
@@ -0,0 +1,168 @@
+package Assignment;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
+import Authentication.LoginService;
+import Clustering.ForumService;
+import app.IssueTracker;
+import models.Cluster;
+import models.Cluster.IssueStatus;
+import models.User;
+import models.UserRole;
+import models.UserStatus;
+
+public class AssignmentTest {
+	private User u;
+    private LoginService auth;
+    private DBCollection dbCollection;
+    private ForumService forum;
+
+    Set<Integer> postIDs = new HashSet<>();
+    IssueTracker issueTracker;
+    Datastore ds;
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+    
+    @Before
+    public void setUpUserAuthenticationMockObjects() throws UnknownHostException{
+    	MongoClient connection = mock(MongoClient.class);
+        Morphia morphia = mock(Morphia.class);
+        ds = mock(Datastore.class);
+
+        when(morphia.createDatastore(any(MongoClient.class),anyString())).thenReturn(ds);
+
+        issueTracker = new IssueTracker(connection, morphia);
+        forum = spy(issueTracker.getForumService());
+        
+        u = spy(new User());
+        when(u.getRole()).thenReturn(UserRole.ADMIN);
+        when(u.getStatus()).thenReturn(UserStatus.LOGIN);
+    }
+    
+    @Test
+    public void administratorCanAssignDeveloperToAnOpenIssue() {
+    	//mock a developer
+    	User developer = mock(User.class);
+    	when(developer.getRole()).thenReturn(UserRole.DEVELOPER);
+    	when(developer.getUsername()).thenReturn("developer1");
+    	
+    	//mock an open issue
+    	Cluster c = spy(new Cluster(0));
+    	when(c.getStatus()).thenReturn(IssueStatus.OPEN);
+    	
+    	assertTrue(u.assignIssue(c, developer));
+    	List<String> assignees = new ArrayList<String>(c.getAssigneeIDs());
+    	assertEquals(assignees.get(0), "developer1");
+    	verify(ds).save(c);
+    }
+    
+    @Test
+    public void administratorCanAssignDeveloperToAnInProgressIssue() {
+    	//mock a developer
+    	User developer = mock(User.class);
+    	when(developer.getRole()).thenReturn(UserRole.DEVELOPER);
+    	when(developer.getUsername()).thenReturn("developer1");
+    	
+    	//mock an open issue
+    	Cluster c = spy(new Cluster(0));
+    	when(c.getStatus()).thenReturn(IssueStatus.IN_PROGRESS);
+    	
+    	assertTrue(u.assignIssue(c, developer));
+    	List<String> assignees = new ArrayList<String>(c.getAssigneeIDs());
+    	assertEquals(assignees.get(0), "developer1");
+    	verify(ds).save(c);
+    }
+    
+    @Test
+    public void administratorCannotAssignDeveloperToClosedIssue() {
+    	//mock a developer
+    	User developer = mock(User.class);
+    	when(developer.getRole()).thenReturn(UserRole.DEVELOPER);
+    	when(developer.getUsername()).thenReturn("developer1");
+    	
+    	//mock an open issue
+    	Cluster c = spy(new Cluster(0));
+    	when(c.getStatus()).thenReturn(IssueStatus.CLOSED);
+    	
+    	u.assignIssue(c, developer);
+    	   	
+    	exception.expect(IssueAlreadyClosedException.class);
+        exception.expectMessage("The issue has already been closed");
+    }
+    
+    @Test
+    public void developerCannotAssignDeveloperToAnIssue() {
+    	when(u.getRole()).thenReturn(UserRole.DEVELOPER);
+    	
+    	//mock a developer
+    	User developer = mock(User.class);
+    	
+    	//mock an open issue
+    	Cluster c = spy(new Cluster(0));
+    	when(c.getStatus()).thenReturn(IssueStatus.OPEN);
+    	
+    	u.assignIssue(c, developer);
+    	   	
+    	exception.expect(PermissionDeniedException.class);
+        exception.expectMessage("You do not have the permission to perform this operation");
+    }
+    
+    @Test
+    public void adminstratorCannotAssignDeveloperAlreadyAssignedToTheSameIssue() {   	
+    	//mock a developer
+    	User developer = mock(User.class);
+    	when(developer.getRole()).thenReturn(UserRole.DEVELOPER);
+    	when(developer.getUsername()).thenReturn("developer1");
+    	
+    	//mock an open issue
+    	Cluster c = spy(new Cluster(0));
+    	when(c.getStatus()).thenReturn(IssueStatus.IN_PROGRESS);
+    	when(c.getAssigneeIDs()).thenReturn(new HashSet<String>(Arrays.asList(developer.getUsername())));
+    	
+    	u.assignIssue(c, developer);
+    	   	
+    	exception.expect(DeveloperAlreadyAssignedException.class);
+        exception.expectMessage("The developer has already been assigned to the issue");
+    }
+    
+    @Test
+    public void adminstratorCannotAssignDeveloperAlreadyAssignedToTheSameIssue() {   	
+    	//mock a developer
+    	User admin = mock(User.class);
+    	when(admin.getRole()).thenReturn(UserRole.ADMIN);
+    	when(admin.getUsername()).thenReturn("admin1");
+    	
+    	//mock an open issue
+    	Cluster c = spy(new Cluster(0));
+    	when(c.getStatus()).thenReturn(IssueStatus.OPEN);
+    	
+    	u.assignIssue(c, admin);
+    	   	
+    	exception.expect(AdminCannotBeenAssignedException.class);
+        exception.expectMessage("An administrator cannot been assigned to an issue");
+    }
+}
